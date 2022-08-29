87edb71e8ae76b31dce20594b0efc45b3e12cefc
Sijtsma, Nico <nico.sijtsma@bakerhughes.com>
2022-08-29 09:59:23 +0200

    generated changes

--- ApiCheck.Console/Properties/AssemblyInfo.cs
+++ ApiCheck.Console/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ApiCheckConsole")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ise Individuelle Software-Entwicklung GmbH")]
-[assembly: AssemblyProduct("ApiCheck.Console")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
--- ApiCheck/Properties/AssemblyInfo.cs
+++ ApiCheck/Properties/AssemblyInfo.cs
@@ -2,15 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ApiCheck")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ise Individuelle Software-Entwicklung GmbH")]
-[assembly: AssemblyProduct("ApiCheck")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
 [assembly: Guid("a0327a29-813c-4517-8a1f-a7f3b71aa860")]
