5e730387b0aa0d5cd91fca5014a08dd7c9b9dc49
Sijtsma, Nico <nico.sijtsma@bakerhughes.com>
2022-08-29 10:09:25 +0200

    manual updates

--- ApiCheck.Test.Project/Properties/AssemblyInfo.cs
+++ ApiCheck.Test.Project/Properties/AssemblyInfo.cs
@@ -1,11 +1,6 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a9e10d02-36ae-4888-9149-ed63d81f5c16")]
 #endif
--- ApiCheck.Test/Properties/AssemblyInfo.cs
+++ ApiCheck.Test/Properties/AssemblyInfo.cs
@@ -1,10 +1,5 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2d7e2a0d-4013-45a2-bc90-ebc6b305c347")]
